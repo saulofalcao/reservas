ff427b4fb745a3b0b1ecb1afdfa884c536d4baeb
Saulo Falcão <saulofalcao@gmail.com>
2013-12-12 21:59:41 -0500

    Apagados os arquivos desnecessarios - da app anterior

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ControleDeEstoque::Application.routes.draw do
 
   root "static_pages#home"
 
-  resources :remedios
+  
   # get "static_pages/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
